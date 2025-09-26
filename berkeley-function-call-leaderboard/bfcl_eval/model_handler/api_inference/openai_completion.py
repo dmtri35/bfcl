133eb8d989a33c6c8e96442f52111f07c08dc279
Tri Dao <daominhtri0503@gmail.com>
2025-09-26 10:21:28 -0400

    conflicts

--- berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/openai_completion.py
+++ berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/openai_completion.py
@@ -22,7 +22,7 @@ from openai import OpenAI, RateLimitError
 class OpenAICompletionsHandler(BaseHandler):
     def __init__(self, model_name, temperature) -> None:
         super().__init__(model_name, temperature)
-        self.model_style = ModelStyle.OpenAI_Completions
+        self.model_style = ModelStyle.OPENAI_COMPLETIONS
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url="http://localhost:8000/v1")
 
     def decode_ast(self, result, language, has_tool_call_tag):
